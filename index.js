1a84bb14371ec3114e134d72046fc5d1af96d1a6
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2020-09-30 14:56:20 +0200

    pink szöveg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var text = document.getElementById('text');
 var list = [
     'Vííí!', 'Soha ne add fel!', 'Az idő mindent megold. Komolyan, csak gondolj bele!',
     'Egyszer egy barátom azt mondta: Never don\'t give up! Kicsit összezavarodott, de a motiváció megvan.',
-    'Szép vagy 😝', '🌈', 'Yeet magyarul: Sutty', '👉👈', '✨',
+    'Szép vagy 😝', '🌈', 'Yeet magyarul: Sutty', '👉👈', '✨', 'yourmom\n{ width: 99999px; }',
     'coder girl (noun)\nA girl with magical powers who can hack your day.\nSynonyms: sorceress, witch, fairy, Luca'
 ];
 
