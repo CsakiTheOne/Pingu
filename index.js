40ed867aaf3b649fa41e84ca9986cd8bb2ebe207
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2020-09-30 14:13:18 +0200

    Új elemek a listába

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var text = document.getElementById('text');
 var list = [
     'Vííí!', 'Soha ne add fel!', 'Az idő mindent megold. Komolyan, csak gondolj bele!',
     'Egyszer egy barátom azt mondta: Never don\'t give up! Kicsit összezavarodott, de a motiváció megvan.',
-    'Szép vagy 😝', '🌈', 'Yeet magyarul: Sutty'
+    'Szép vagy 😝', '🌈', 'Yeet magyarul: Sutty', '👉👈', '✨',
+    'coder girl (noun)\nA girl with magical powers who can hack your day.\nSynonyms: sorceress, witch, fairy, Luca'
 ];
 
 document.getElementById('fab').addEventListener('click', () => {
